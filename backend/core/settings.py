efebd3327234e2b3180ac45130f2f7c49f1c2b89
Andrew <andrewsafonovpost@gmail.com>
2025-05-27 21:56:41 +0300

    #3 Configured static files, added welcome template

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -49,7 +49,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,6 +108,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 
---
+++ backend/static/js/typed.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Typed('#typed-text', {
+        strings: ['GalaxPeak', 'Secure. Fast. Cosmic.'],
+        typeSpeed: 70,
+        backSpeed: 40,
+        backDelay: 2000,
+        loop: true
+    });
+});
